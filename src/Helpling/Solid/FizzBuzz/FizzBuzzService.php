ff6def856d7ae2e3fad7a249873dde9fdb33ff91
mfranczakhelpling <michal.franczak@helpling.com>
2015-10-22 23:00:38 +0200

    FizzBuzz task entry point

---
+++ src/Helpling/Solid/FizzBuzz/FizzBuzzService.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @author Michal Franczak <michal.franczak@helpling.com>
+ * @link
+ */
+
+namespace Helpling\Solid\FizzBuzz;
+
+
+class FizzBuzzService
+{
+    /**
+     * Generate a list of integers, from 1 to n.  
+     *
+     * @param int $n
+     * @return array
+     */
+    public function generate($n)
+    {
+        return [];
+    }
+}
